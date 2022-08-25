824281663cb398dbea0ad1ea515fa95ba33cb594
Jakub Rogowski <jakerogowski33gmail.com>
2022-08-25 12:52:41 +0200

    Added base Book entity

--- src/main/java/com/ormanager/client/entity/Book.java
+++ src/main/java/com/ormanager/client/entity/Book.java
@@ -1,4 +1,28 @@
 package com.ormanager.client.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Table("books")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Book {
+    @Id
+    private Long id;
+
+    @Column
+    @NonNull
+    private String title;
+
+    @Column("published_at")
+    @NonNull
+    private LocalDate publishedAt;
+
+    @ManyToOne(columnName = "publisher_id")
+    private Publisher publisher = null;
 }
