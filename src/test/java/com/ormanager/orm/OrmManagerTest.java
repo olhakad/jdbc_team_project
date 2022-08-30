a8028ed1995f2e7d68880f6b0ad71c888200710a
bsyso <bsyso@softserveinc.com>
2022-08-30 11:18:55 +0200

    added tests structure

---
+++ src/test/java/com/ormanager/orm/OrmManagerTest.java
@@ -0,0 +1,52 @@
+package com.ormanager.orm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrmManagerTest {
+
+    @Test
+    void getConnection() {
+    }
+
+    @Test
+    void persist() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getTableClassName() {
+    }
+
+    @Test
+    void getAllDeclaredFieldsFromObject() {
+    }
+
+    @Test
+    void getAllValuesFromListToString() {
+    }
+
+    @Test
+    void getAllValuesFromObject() {
+    }
+
+    @Test
+    void getAllColumnsButId() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findAllAsStream() {
+    }
+}
