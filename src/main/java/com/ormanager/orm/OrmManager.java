331f5bcebff5687f7881df818736ff2d30428b73
Jakub Dudek <jdude@softserveinc.com>
2022-08-30 11:47:21 +0000

    OrmManager updated from Register_classes_and_create_DDL branch #UPDATE

--- src/main/java/com/ormanager/orm/OrmManager.java
+++ src/main/java/com/ormanager/orm/OrmManager.java
@@ -3,6 +3,7 @@ package com.ormanager.orm;
 import com.ormanager.jdbc.DataSource;
 import com.ormanager.orm.annotation.Column;
 import com.ormanager.orm.annotation.Id;
+import com.ormanager.orm.annotation.ManyToOne;
 import com.ormanager.orm.annotation.Table;
 import com.ormanager.orm.exception.OrmFieldTypeException;
 import com.ormanager.orm.mapper.ObjectMapper;
@@ -10,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Date;
@@ -189,7 +191,9 @@ public class OrmManager<T> {
         StringBuilder registerSQL = new StringBuilder("CREATE TABLE IF NOT EXISTS " + tableName +
                 " (" + id.getName() + " int UNSIGNED AUTO_INCREMENT,"
                 + columnNamesAndTypes
-                + " PRIMARY KEY (" + id.getName() + "))");
+                + " PRIMARY KEY (" + id.getName() + ")");
+
+        registerSQL.append(")");
 
         logger.info("CREATE TABLE SQL statement is being prepared now: " + registerSQL);
 
@@ -199,7 +203,50 @@ public class OrmManager<T> {
         logger.info("CREATE TABLE SQL completed successfully! {} entity has been created in DB.", tableName.toUpperCase());
     }
 
+    public void createRelationships(Class<?>... entityClasses) throws SQLException {
+        for (var entity : entityClasses) {
+            if (doesClassHaveAnyRelationship(entity)) {
+                createRelationships(entity);
+            }
+        }
+    }
+
+    private void createRelationships(Class<?> clazz) throws SQLException {
+        for (var field : getRelationshipFields(clazz, ManyToOne.class)) {
+
+            var fieldClass = field.getType();
+            var fieldClassName = getTableName(fieldClass);
+            var fieldClassIdName = getIdField(fieldClass).getName();
+
+            if (doesEntityExists(fieldClass) && !(doesRelationshipAlreadyExist(clazz, fieldClass))) {
+
+                var relationshipSQL = "ALTER TABLE " + getTableName(clazz) + " ADD COLUMN " + fieldClassName + "_id int UNSIGNED," +
+                        " ADD FOREIGN KEY (" + fieldClassName + "_id)" +
+                        " REFERENCES " + fieldClassName + "(" + fieldClassIdName + ") ON DELETE CASCADE;";
+
+                logger.info("Establishing relationship between entities: {} and {} is being processed now: " + relationshipSQL,
+                        clazz.getSimpleName().toUpperCase(), fieldClassName.toUpperCase());
+
+                PreparedStatement statement = con.prepareStatement(relationshipSQL);
+                statement.execute();
+
+                logger.info("Establishing relationship processed successfully!");
+
+            } else {
+                if (!doesEntityExists(fieldClass)) {
+                    var missingEntityName = fieldClass.getSimpleName();
+
+                    throw new SQLException(String.format("Relationship between %s and %s cannot be made! Missing entity %s!",
+                            clazz.getSimpleName(), missingEntityName, missingEntityName));
+                }
+                logger.info("Relationship between entities: {} and {} already exists.", clazz.getSimpleName().toUpperCase(), fieldClassName.toUpperCase());
+            }
+        }
+    }
+
     private String getSqlTypeForField(Field field) {
+        field.setAccessible(true);
+
         var fieldType = field.getType();
 
         if (fieldType == String.class) {
@@ -231,11 +278,37 @@ public class OrmManager<T> {
                 .orElseThrow(() -> new SQLException(String.format("ID field not found in class %s !", clazz)));
     }
 
+    private List<Field> getRelationshipFields(Class<?> clazz, Class<? extends Annotation> relationAnnotation) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(relationAnnotation))
+                .toList();
+    }
+
+    private boolean doesRelationshipAlreadyExist(Class<?> clazzToCheck, Class<?> relationToCheck) throws SQLException {
+        String findRelationSQL = "SELECT REFERENCED_TABLE_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = '" + getTableName(clazzToCheck) + "';";
+
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery(findRelationSQL);
+        resultSet.next();
+
+        while (resultSet.next()) {
+            if (resultSet.getString(1).equals(getTableName(relationToCheck))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean doesClassHaveAnyRelationship(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .anyMatch(field -> field.isAnnotationPresent(ManyToOne.class));
+    }
+
     private boolean doesEntityExists(Class<?> clazz) throws SQLException {
         var searchedEntityName = getTableName(clazz);
 
         String checkIfEntityExistsSQL = "SELECT COUNT(*) FROM information_schema.TABLES " +
-                                        "WHERE (TABLE_SCHEMA = 'test') AND (TABLE_NAME = '" + searchedEntityName + "');";
+                "WHERE (TABLE_SCHEMA = 'test') AND (TABLE_NAME = '" + searchedEntityName + "');";
 
         Statement statement = con.createStatement();
         ResultSet resultSet = statement.executeQuery(checkIfEntityExistsSQL);
