8e8140a6f0af2eaee92d6604bc5418e64533d82b
Robert Michalski <rmicha@softserveinc.com>
2022-09-01 11:31:50 +0200

    add save test

--- src/main/java/com/ormanager/App.java
+++ src/main/java/com/ormanager/App.java
@@ -1,5 +1,7 @@
 package com.ormanager;
 
+import com.ormanager.client.entity.Publisher;
+import com.ormanager.orm.OrmManager;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationTargetException;
@@ -9,5 +11,6 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) throws SQLException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         LOGGER.info("Welcome to our ORManager impl!");
+        OrmManager ormManager = OrmManager.getConnection();
     }
 }
