ea146ef63a37eccf30302d8d126f247f0331725f
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-02 13:50:31 +0200

    clean up the mess in App

--- src/main/java/com/ormanager/App.java
+++ src/main/java/com/ormanager/App.java
@@ -1,8 +1,5 @@
 package com.ormanager;
 
-import com.ormanager.client.entity.Publisher;
-import com.ormanager.orm.ClassScanner;
-import com.ormanager.orm.OrmManager;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.InvocationTargetException;
@@ -12,11 +9,5 @@ import java.sql.SQLException;
 public class App {
     public static void main(String[] args) throws SQLException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         LOGGER.info("Welcome to our ORManager impl!");
-        OrmManager ormManager = OrmManager.getConnection();
-        var entityClassesAsSet = ClassScanner.getClassesMarkedAsEntity();
-        var entityClassesAsArray = new Class<?>[entityClassesAsSet.size()];
-        entityClassesAsSet.toArray(entityClassesAsArray);
-        OrmManager.getConnection().register(entityClassesAsArray);
-        OrmManager.getConnection().createRelationships(entityClassesAsArray);
     }
 }
