e5de7d9d90c3b8b9da5d0553d76d70d159c2009d
Jakub Dudek <jdude@softserveinc.com>
2022-09-04 14:46:44 +0000

    Logic of auto register/createRelations for entities closed in one method #REFACTOR

--- src/main/java/com/ormanager/App.java
+++ src/main/java/com/ormanager/App.java
@@ -4,14 +4,17 @@ import com.ormanager.orm.ClassScanner;
 import com.ormanager.orm.OrmManager;
 import lombok.extern.slf4j.Slf4j;
 
-import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 @Slf4j(topic = "AppTest")
 public class App {
-    public static void main(String[] args) throws SQLException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws SQLException {
         LOGGER.info("Welcome to our ORManager impl!");
 
+        initializeEntitiesAndRelations();
+    }
+
+    private static void initializeEntitiesAndRelations() throws SQLException {
         var ormManager = OrmManager.withPropertiesFrom("src/main/resources/application.properties");
         var entityClassesAsSet = ClassScanner.getClassesMarkedAsEntity();
         var entityClassesAsArray = new Class<?>[entityClassesAsSet.size()];
