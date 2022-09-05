f305bce24982de9fb16401a0aa056e74c2ef62df
bsyso <bsyso@softserveinc.com>
2022-09-05 07:25:16 +0200

    added fixing mockito tests

--- src/test/java/com/ormanager/orm/OrmBookManagerTest.java
+++ src/test/java/com/ormanager/orm/OrmBookManagerTest.java
@@ -1,33 +1,38 @@
 package com.ormanager.orm;
 
 import com.ormanager.client.entity.Book;
-import com.ormanager.client.entity.Publisher;
-import com.ormanager.jdbc.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Optional;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 @Slf4j
 class OrmBookManagerTest {
+    @Mock
+    private DataSource underTestDataSource;
+    @Mock
+    private Connection underTestConnection;
+    @Mock
+    private PreparedStatement underTestPreparedStatement;
+    @Mock
+    private ResultSet underTestResultSet;
     @InjectMocks
-    private OrmManager underTestOrmManager = OrmManager.getConnection();
-
-    OrmBookManagerTest() throws SQLException {
-    }
+    private OrmManager<Book> underTestOrmManager;
 
     @BeforeEach
     void setUp() {
@@ -36,54 +41,14 @@ class OrmBookManagerTest {
     }
 
     @Test
-    void persistTest_ShouldSaveBook() throws IllegalAccessException, SQLException {
-        //Given
-        Book book = new Book(1L, "test", LocalDate.now());
-
-        //When
-        underTestOrmManager.persist(book);
-
-        //Then
-        verify(underTestOrmManager, atLeastOnce()).persist(book);
-    }
-
-    @Test
-    void saveTest_ShouldSaveAndReturnBook() throws IllegalAccessException, SQLException {
-        //Given
-        Book book = new Book(1L, "test", LocalDate.now());
-
-        //When
-        when(underTestOrmManager.save(book)).thenReturn(book);
-        underTestOrmManager.save(book);
-
-        //Then
-        verify(underTestOrmManager, atLeastOnce()).save(book);
-    }
-
-    @Test
-    void findByIdTest_ShouldReturnBookById() {
-        //Given
-        Book book = new Book(1L, "test", LocalDate.now());
-
-        //When
-        when(underTestOrmManager.findById(book.getId(), Book.class)).thenReturn(Optional.of(book));
-        var result = underTestOrmManager.findById(book.getId(), Book.class).orElseThrow();
-
-        //Then
-        verify(underTestOrmManager, atLeastOnce()).findById(book.getId(), Book.class);
-        assertEquals(result, book);
-    }
-
-    @Test
     void findAllTest_ShouldReturnListOfBooks() throws SQLException {
         //Given
-        given(underTestOrmManager.findAll(Book.class)).willReturn(new ArrayList<>());
+        given(underTestConnection.createStatement().executeQuery("select * from books")).willReturn(underTestResultSet);
 
         //When
         var expected = underTestOrmManager.findAll(Book.class);
 
         //Then
-        verify(underTestOrmManager, atLeastOnce()).findAll(Book.class);
+        assertTrue(expected.size() > 0);
     }
-
 }
