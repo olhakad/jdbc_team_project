0a6c7bf0ee3ad70b3d53bd6cbf16e09f56cdaa29
Jakub Dudek <jdude@softserveinc.com>
2022-09-06 09:26:45 +0000

    App - minor update to exceptions in signatures of methods #UPDATE

--- src/main/java/com/ormanager/App.java
+++ src/main/java/com/ormanager/App.java
@@ -8,13 +8,13 @@ import java.sql.SQLException;
 
 @Slf4j(topic = "AppTest")
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, NoSuchFieldException {
         LOGGER.info("Welcome to our ORManager impl!");
 
         initializeEntitiesAndRelations();
     }
 
-    private static void initializeEntitiesAndRelations() throws SQLException {
+    private static void initializeEntitiesAndRelations() throws SQLException, NoSuchFieldException {
         var ormManager = OrmManager.withPropertiesFrom("src/main/resources/application.properties");
         var entityClassesAsSet = ClassScanner.getClassesMarkedAsEntity();
         var entityClassesAsArray = new Class<?>[entityClassesAsSet.size()];
