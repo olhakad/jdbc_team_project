3b79a4a7c213ac1cade816848cbd604429af4b50
bsyso <bsyso@softserveinc.com>
2022-09-06 11:49:04 +0200

    added need tests fixing

--- src/main/java/com/ormanager/orm/ClassScanner.java
+++ src/main/java/com/ormanager/orm/ClassScanner.java
@@ -2,16 +2,16 @@ package com.ormanager.orm;
 
 import com.ormanager.orm.annotation.Entity;
 import lombok.extern.slf4j.Slf4j;
-//import org.reflections.Reflections;
+import org.reflections.Reflections;
 
 import java.util.Set;
 
 @Slf4j(topic = "ClassScanner")
 public class ClassScanner {
 
-    /*public static Set<Class<?>> getClassesMarkedAsEntity() {
+    public static Set<Class<?>> getClassesMarkedAsEntity() {
         Reflections reflections = new Reflections("com.ormanager.client.entity");
 
         return reflections.getTypesAnnotatedWith(Entity.class);
-    }*/
+    }
 }
