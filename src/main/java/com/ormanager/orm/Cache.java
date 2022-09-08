e7b2e6e79011b45b2205e07bd8d9791f007d322a
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-08 10:02:33 +0200

    Code reformat according to Olha comments

--- src/main/java/com/ormanager/orm/Cache.java
+++ src/main/java/com/ormanager/orm/Cache.java
@@ -39,6 +39,7 @@ class Cache {
     }
 
     <T> Optional<T> getFromCache(Serializable recordId, Class<T> clazz) {
+
         var retrievedRecord = cacheMap.get(clazz).get(recordId);
 
         if (recordId != null) {
@@ -50,11 +51,13 @@ class Cache {
     }
 
     List<Object> getAllFromCache(Class<?> clazz) {
+
         var values = cacheMap.get(clazz).values();
         return Arrays.asList(values.toArray());
     }
 
-    boolean deleteFromCache(Object recordToDelete) throws IllegalAccessException {
+    void deleteFromCache(Object recordToDelete) throws IllegalAccessException {
+
         Serializable recordId = getRecordId(recordToDelete);
         Class<?> keyClazz = recordToDelete.getClass();
 
@@ -67,45 +70,46 @@ class Cache {
                         Collection<Object> values = getChildren(recordId, childKey);
                         return values.stream();
                     })
-                    .forEach(o -> {
-                        Field childIdField = OrmManagerUtil.getIdField(o).get();
-                        childIdField.setAccessible(true);
-                        try {
-                            childIdField.set(o, null);
-                        } catch (IllegalAccessException e) {
-                            LOGGER.error(e.getMessage(), "When setting child's ID to null");
-                        }
-                    });
+                    .forEach(this::setObjectIdToNull);
+
             LOGGER.info("{}'s (id = {}) all children's ids set to null", recordToDelete.getClass().getSimpleName(), recordId);
         }
 
         setObjectIdToNull(recordToDelete);
 
-        return cacheMap.get(keyClazz).remove(recordId, recordToDelete);
-    }
-
-    private void setObjectIdToNull(Object object) throws IllegalAccessException {
-        Field objectIdField = OrmManagerUtil.getIdField(object).get();
-        objectIdField.setAccessible(true);
-        objectIdField.set(object, null);
-        LOGGER.info("{}'s id set to null", object.getClass().getSimpleName());
+        cacheMap.get(keyClazz).remove(recordId, recordToDelete);
     }
 
     boolean isRecordInCache(Serializable recordId, Class<?> clazz) {
+
         return Optional.ofNullable(cacheMap.get(clazz))
                 .map(m -> m.containsKey(recordId))
                 .orElse(false);
+    }
+
+    private void setObjectIdToNull(Object object) {
+
+        OrmManagerUtil.getIdField(object).ifPresent(field -> {
+            field.setAccessible(true);
+            try {
+                field.set(object, null);
+            } catch (IllegalAccessException e) {
+                LOGGER.error(e.getMessage(), "When setting id field to null");
+            }
+        });
+
+        LOGGER.info("{}'s id set to null", object.getClass().getSimpleName());
     }
 
     private Serializable getRecordId(Object t) throws IllegalAccessException {
         Optional<Field> optionalId = Arrays.stream(t.getClass().getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(Id.class))
                 .findFirst();
-        if (optionalId.isPresent()) {
-            optionalId.get().setAccessible(true);
-            return (Serializable) optionalId.get().get(t);
-        }
-        return null;
+
+        if (optionalId.isEmpty()) return null;
+
+        optionalId.get().setAccessible(true);
+        return (Serializable) optionalId.get().get(t);
     }
 
     /**
@@ -126,7 +130,7 @@ class Cache {
             try {
                 field.set(retrievedRecord, values);
             } catch (IllegalAccessException e) {
-                LOGGER.error(e.getMessage() + "Assigning list of children to parent");
+                LOGGER.error(e.getMessage(), "When assigning list of children to parent");
             }
         });
     }
@@ -144,32 +148,41 @@ class Cache {
         try {
             childKey = Class.forName(childKeyString);
         } catch (ClassNotFoundException e) {
-            LOGGER.trace(e.getMessage() + "dark magic successfully casted. Application is no more");
+            LOGGER.trace(e.getMessage(), "Dark magic successfully casted. Application is no more");
         }
         return childKey;
     }
 
     /**
-     * Looks for children is cache
+     * Looks for children in cache
      *
      * @param recordId is a parent's id
      * @param childKey is a key to go through map
      * @return collection of children
      */
     private Collection<Object> getChildren(Serializable recordId, Class<?> childKey) {
+
         Collection<Object> values = cacheMap.get(childKey).values();
         values = values.stream().filter(value -> {
-            Optional<Field> any = Arrays.stream(value.getClass().getDeclaredFields()).filter(field1 -> field1.isAnnotationPresent(ManyToOne.class)).findAny();
-            Field parentField = any.get();
+            Optional<Field> childCollection = Arrays
+                    .stream(value.getClass().getDeclaredFields())
+                    .filter(field -> field.isAnnotationPresent(ManyToOne.class))
+                    .findAny();
+
+            if (childCollection.isEmpty()) return false;
+
+            Field parentField = childCollection.get();
+
             parentField.setAccessible(true);
             try {
                 Serializable id = OrmManagerUtil.getId(parentField.get(value));
                 return id == recordId;
             } catch (IllegalAccessException e) {
-                LOGGER.error(e.getMessage() + "Getting ID for parent");
+                LOGGER.error(e.getMessage(), "When getting ID for parent");
+                return false;
             }
-            return false;
         }).toList();
+
         return values;
     }
 
