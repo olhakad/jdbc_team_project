ff4c5d1594328549ddf3e84745e04b9fe2311502
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-14 12:48:01 +0200

    count test

--- src/test/java/com/ormanager/orm/CacheTest.java
+++ src/test/java/com/ormanager/orm/CacheTest.java
@@ -4,11 +4,14 @@ import com.ormanager.client.entity.Book;
 import com.ormanager.client.entity.Publisher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CacheTest {
 
@@ -24,6 +27,7 @@ class CacheTest {
 
     @BeforeEach
     void setUp() {
+
         bookOne = new Book(1L, "Book One", LocalDate.now().minusYears(10), publisherOne);
         bookTwo = new Book(2L, "Book One", LocalDate.now().minusYears(3), publisherOne);
         bookThree = new Book(3L, "Book One", LocalDate.now().minusYears(12), publisherTwo);
@@ -35,7 +39,7 @@ class CacheTest {
         testable.putToCache(publisherOne);
         publisherOne.getBooks().forEach(testable::putToCache);
 
-        testable.putToCache(publisherOne);
+        testable.putToCache(publisherTwo);
         publisherTwo.getBooks().forEach(testable::putToCache);
     }
 
@@ -45,11 +49,20 @@ class CacheTest {
     }
 
     @Test
-    void count() {
+    @DisplayName("COUNT: Should return correct count of Publishers (2) and Books (4) that are stored in cache.")
+    void countTest() {
 
         // given
+        Long publishersCount;
+        Long booksCount;
+
         // when
+        publishersCount = testable.count(Publisher.class);
+        booksCount = testable.count(Book.class);
+
         // then
+        assertThat(publishersCount).isEqualTo(2);
+        assertThat(booksCount).isEqualTo(4);
     }
 
     @Test
